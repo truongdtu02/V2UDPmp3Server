be61dd6c54ab66c5fd7177f828a844e068e63f43
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-04-24 15:29:20 +0700

    update song

--- V2UDPmp3Server/Program.cs
+++ V2UDPmp3Server/Program.cs
@@ -41,9 +41,9 @@ namespace V2UDPmp3Server
 
             List<soundTrack> soundListServer = new List<soundTrack>()
             {
-                //new soundTrack(){ FilePath = "bai48k.mp3"},
-                //new soundTrack(){ FilePath = "bai24k.mp3"},
-                //new soundTrack(){ FilePath = "bai16k.mp3"},
+                new soundTrack(){ FilePath = "bai48k.mp3"},
+                new soundTrack(){ FilePath = "bai24k.mp3"},
+                new soundTrack(){ FilePath = "bai16k.mp3"},
                 new soundTrack(){ FilePath = "bai8k.mp3"}
                 //new soundTrack(){ FilePath = "LoveIsBlue.mp3"}
             };
